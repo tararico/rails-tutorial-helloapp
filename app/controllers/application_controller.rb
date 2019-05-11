7d4ab0f8ac24217eae0632c2eb7f5b6a2e30414e
tararico <emipu.uq@gmail.com>
2019-05-11 15:18:04 +0900

    change method&root

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-  def hello
-    render html: "hello,world!"
+  def goodbye
+    render html: "goodbye,world!"
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file,
   # see http://railsguides.jp/routing.html
-  root 'application#hello'
+  root 'application#goodbye'
 end
